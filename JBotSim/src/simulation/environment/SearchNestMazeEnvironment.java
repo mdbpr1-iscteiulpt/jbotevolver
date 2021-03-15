1dc1730af5ac7c0bea1aa18534833e87bb6a8943
mdbpr1-iscteiulpt <32898777+mdbpr1-iscteiulpt@users.noreply.github.com>
2021-03-15 19:15:07 +0000

    Finishing Test 3

--- JBotSim/src/simulation/environment/SearchNestMazeEnvironment.java
+++ JBotSim/src/simulation/environment/SearchNestMazeEnvironment.java
@@ -4,6 +4,7 @@ import java.util.Random;
 
 import mathutils.VectorLine;
 import simulation.Simulator;
+import simulation.physicalobjects.Nest;
 import simulation.physicalobjects.Wall;
 import simulation.robot.Robot;
 import simulation.util.ArgumentsAnnotation;
@@ -16,6 +17,8 @@ public class SearchNestMazeEnvironment extends Environment{
 	
 	private int numberOfRobotsInNest = 0;
 	private int numberOfRobotsThatCollided = 0;
+	
+	private Nest nest;
 	
 	@ArgumentsAnnotation(name="DisableWalls", values={"0","1"})	
 	private boolean DisableWalls = false;
@@ -42,6 +45,9 @@ public class SearchNestMazeEnvironment extends Environment{
 				robot.setPosition(0,-2.5,0);
 			}
 		}
+		
+		this.random = simulator.getRandom();
+		
 		//SetupWall
 		if(!DisableWalls) {
 			addWall(new Wall(simulator, new VectorLine(2,0,0), 0.2,100,100,0));
@@ -53,25 +59,28 @@ public class SearchNestMazeEnvironment extends Environment{
 		//RandomWallSpawning
 		if(!DisableWalls) {
 			double[] xpos = {1.5,1.25,1,0.75,0.5};
-			for(int i = 1; i>=-1; i--) {
-				int randomPos = random.nextInt(5) - 1;
+			for(double i = 1.5; i>=-1.5; i--) {
+				int randomPos = random.nextInt(5);
 				System.out.println(randomPos);
-				/*if(randomPos == 0)*/if(i == 1) {
+				if(randomPos == 0){
 					addWall(new Wall(simulator, new VectorLine(1.625,i,0), 0.75,0.25,100,0)); addWall(new Wall(simulator, new VectorLine(-0.5,i,0), 2.5,0.25,100,0));			
 				}
-				/*if(randomPos == 1) {
-					addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0)); addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0));		
+				if(randomPos == 1) {
+					addWall(new Wall(simulator, new VectorLine(-1.3,i,0), 1.4,0.25,100,0)); addWall(new Wall(simulator, new VectorLine(0.9,i,0), 2,0.25,100,0));			
 				}
 				if(randomPos == 2) {
-					addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0)); addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0));					
+					addWall(new Wall(simulator, new VectorLine(0,i+0.5,0), 0.2,1,100,0));
+					addWall(new Wall(simulator, new VectorLine(0,i,0), 2.5,0.25,100,0));	
 				}
 				if(randomPos == 3) {
-					addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0)); addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0));			
+					addWall(new Wall(simulator, new VectorLine(1,i+0.5,0), 0.2,1,100,0));
+					addWall(new Wall(simulator, new VectorLine(-1,i,0), 2,0.25,100,0));	
 				}
 				if(randomPos == 4) {
-					addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0)); addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0));				
+					addWall(new Wall(simulator, new VectorLine(-1,i+0.5,0), 0.2,1,100,0));
+					addWall(new Wall(simulator, new VectorLine(1,i,0), 2,0.25,100,0));	
 				}
-				if(randomPos == 5) {
+				/*if(randomPos == 5) {
 					addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0)); addWall(new Wall(simulator, new VectorLine(2,i,0), x,1,100,0));				
 				}
 				if(randomPos == 6) {
@@ -79,6 +88,10 @@ public class SearchNestMazeEnvironment extends Environment{
 				}	*/			
 			}
 		}
+		
+		//Add Nest
+		nest = new Nest(simulator, "Nest", 0, 0, 0.5);
+		addObject(nest);
 	}
 	
 	@Override
